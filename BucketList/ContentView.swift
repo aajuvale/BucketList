b10aaaed56e78b7612351fc290f1815bff469b64
aajuvale <ahmed.juvale@gmail.com>
2025-08-17 11:00:43 -0700

    feat: implemented MVVM and saving data

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -17,15 +17,12 @@ struct ContentView: View {
     )
 
     @State
-    private var locations = [Location]()
-
-    @State
-    private var selectedPlace: Location?
+    private var viewModel = ViewModel()
 
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
-                ForEach(locations) { location in
+                ForEach(viewModel.locations) { location in
                     Annotation(location.name, coordinate: location.coordinate) {
                         Image(systemName: "star.circle")
                             .resizable()
@@ -34,28 +31,19 @@ struct ContentView: View {
                             .background(.white)
                             .clipShape(.circle)
                             .onLongPressGesture {
-                                selectedPlace = location
+                                viewModel.selectedPlace = location
                             }
                     }
                 }
             }
             .onTapGesture { position in
                 if let coordinate = proxy.convert(position, from: .local) {
-                    let newLocation = Location(
-                        id: UUID(),
-                        name: "New location",
-                        description: "",
-                        latitude: coordinate.latitude,
-                        longitude: coordinate.longitude
-                    )
-                    locations.append(newLocation)
+                    viewModel.addLocation(at: coordinate)
                 }
             }
-            .sheet(item: $selectedPlace) { place in
+            .sheet(item: $viewModel.selectedPlace) { place in
                 EditView(location: place) { newLocation in
-                    if let index = locations.firstIndex(of: place) {
-                        locations[index] = newLocation
-                    }
+                    viewModel.update(location: newLocation)
                 }
             }
         }
